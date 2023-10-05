b77168e48f131552f0f57c28eec726f22f84ac39
DonyFatahillah <145980954+DonyFatahillah@users.noreply.github.com>
2023-10-05 22:43:54 +0700

    Prak3_Elkom1.py

---
+++ Prak3_Elkom1.py
@@ -0,0 +1,20 @@
+print("\n \n")
+
+print("@@@@   @@@@@   @@  @  @   @")
+print("@   @  @   @   @ @ @   @ @")
+print("@   @  @   @   @  @@    @")
+print("@@@@   @@@@@   @   @    @")
+
+print("\n \n")
+
+
+angka1st = int(input("masukan angka pertama : "))
+angkaLast = int(input("masukan angka terakhir : "))
+
+while angka1st <angkaLast:
+    print(f"{angka1st} | {angkaLast}")
+    angka1st += 1
+    angkaLast -= 1
+
+
+
